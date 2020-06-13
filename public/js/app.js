b1e89470d046f98f1d09b4d0c8462985782b99d9
Nilanjan <nilanjan.ganguly@gmail.com>
2020-06-13 17:33:46 +0530

    Heroku deployment changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ console.log("this is getting called!!")
 handleClick = () => {
   let place = document.querySelector("#searchTxt").value;
   // console.log(place);
-  fetch("http://localhost:3000/weather?search=" + place).then((response) => {
+  fetch("/weather?search=" + place).then((response) => {
     response.json().then((data) => {
       document
         .querySelector("#locationTxt").innerHTML="This is the weather in :  " + data[0].location;
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require("path")
 const hbs = require("hbs")
 const util = require("./utils.js");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
 const SLDSpath = path.join(
